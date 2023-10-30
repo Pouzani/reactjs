76d4216835b7a877170ac03fc6ca7dd9b657fd8b
Mohamed Riyad Bouzani <mriyadbouzani@gmail.com>
2023-10-30 22:39:54 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Row, Container, Col } from "bootstrap-4-react";
+import NavigationBar from "./Components/NavigationBar";
+import Bienvenue from "./Components/Bienvenue";
+import Footer from "./Components/Footer";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Voiture from "./Components/Voiture";
+import VoitureListe from "./Components/VoitureListe";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const marginTop = { marginTop: "20px" };
+	return (
+		<Router>
+			<NavigationBar />
+			<Container>
+				<Row>
+					<Col lg={12} style={marginTop}>
+						<Switch>
+							<Route path="/" exact component={Bienvenue} />
+							<Route path="/add" exact component={Voiture} />
+							<Route path="/edit/:id" exact component={Voiture} />
+							<Route path="/list" exact component={VoitureListe} />
+							
+						</Switch>
+					</Col>
+				</Row>
+			</Container>
+			<Footer />
+		</Router>
+	);
 }
 
 export default App;
